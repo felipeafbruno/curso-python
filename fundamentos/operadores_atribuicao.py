e7acb593434b19023c2240bb07343cedd2645c1c
felipeafbruno <felipedobru@gmail.com>
2021-02-23 18:36:27 -0300

    Exemplo de alguns operadores de atribuição

---
+++ fundamentos/operadores_atribuicao.py
@@ -0,0 +1,24 @@
+a = 3
+a = a + 7 # a tem seu valor substitudo pela soma de 'a' + 7
+print(a)
+
+a += 5 # atribuição aditiva -> forma reduzida para a = a + 5
+print(a)
+
+a -= 2 # atribuição subtrativa -> forma reduzida para a = a - 2
+print(a)
+
+a *= 8 # atribuição multiplicativa -> forma reduzida para a = a * 8
+print(a)
+
+a /= 2 # atribuição de divisivão -> forma reduzida para a = a / 2
+print(a)
+
+a %= 2 # atribuição de módulo -> forma reduzida para a = a % 2
+print(a)
+
+a **= 8 # atribução de exponeciação -> forma reduzida para a = a ** 8
+print(a)
+
+a //= 4 # atribução de divisão com retorno inteiro -> forma reduzida para a = a // 8
+print(a)
