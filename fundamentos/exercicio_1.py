d87ea19cd2bf61ba74fff8404146497a82701a53
felipeafbruno <felipedobru@gmail.com>
2021-02-23 18:22:10 -0300

    Exercício com operação aritmética

---
+++ fundamentos/exercicio_1.py
@@ -0,0 +1,11 @@
+# Operações Aritméticas
+salario = 5600
+despesas = 1250.82
+
+'''
+    Exercício Simples para Calcular o 
+    percentual do salário comprometido 
+    com as despesas.
+'''
+percentual = (despesas * 100) / salario
+print(percentual)
