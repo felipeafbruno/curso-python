154a82387a70f618cce6c560287ff684d763b7cb
felipeafbruno <felipedobru@gmail.com>
2021-02-23 17:57:03 -0300

    Tipos de comentários válidos em python

---
+++ fundamentos/comentarios.py
@@ -0,0 +1,15 @@
+# Minhas variáveis
+salario = 3500.52
+despesas = 2456.42
+
+# comentário
+# de 
+# multiplas
+# linhas
+
+'''
+A idéia é calcular 
+o quanto vai sobrar 
+no final do mês.
+'''
+print(salario - despesas)
