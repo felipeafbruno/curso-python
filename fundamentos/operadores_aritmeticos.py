0cf773f96cf8d270a1b76ec8271b02f9669657d8
felipeafbruno <felipedobru@gmail.com>
2021-02-23 18:20:17 -0300

    Algumas observações sobre operações Aritméticas

---
+++ fundamentos/operadores_aritmeticos.py
@@ -0,0 +1,7 @@
+print(2 + 3)
+print(4 - 7)
+print(2 * 5.3)
+print(9.4 / 3) # resultado: valor float/real
+print(9.4 // 3) # resultado: utilizar // o valor retornado é a parte inteira da operação ainda que convertido para um float
+print(8 ** 2) # resultado: utilizar ** temos uma valor 'x' elevado a potência de 'y' 
+print(10 % 2) # resultado: módulo ou resto de divisão
