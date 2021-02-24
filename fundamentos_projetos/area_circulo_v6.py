545146051b93f3b659c9c79fe04d1659611e5a82
felipeafbruno <felipedobru@gmail.com>
2021-02-24 14:32:07 -0300

    Area da Circunferência versão 6 - modulos

---
+++ fundamentos_projetos/area_circulo_v6.py
@@ -0,0 +1,7 @@
+from math import pi
+
+raio = input('Informe o raio: ')
+area_circunferencia = pi * (float(raio) ** 2)
+print(f'Resultado é = {area_circunferencia}')
+
+print('Nome do módulo ', __name__)
