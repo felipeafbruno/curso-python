b1a9739b0a015b2de0bd5cb71d88fdd5a9a11a77
felipeafbruno <felipedobru@gmail.com>
2021-02-24 14:09:33 -0300

    Area da Circunferência versão 4 - importando pi do módulo Math

---
+++ fundamentos_projetos/area_circulo_v4.py
@@ -0,0 +1,7 @@
+from math import pi
+# print('pi=', math.pi) # utilizando propriedade pi do módulo math
+# print(dir())
+
+raio = 3
+area_circunferencia = pi * (raio ** 2)
+print(f'Resultado é = {area_circunferencia}')
