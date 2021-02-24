9b4de0f76ffd41afbd38c098bc07f408db5b9675
felipeafbruno <felipedobru@gmail.com>
2021-02-24 09:24:23 -0300

    Algumas observações sobre operadores unários

---
+++ fundamentos/operadores_unarios.py
@@ -0,0 +1,18 @@
+'''
+    Para incremento ou decremento de um valor 
+    utilizar as atribuições aditivas e subtrativas.
+'''
+a = 3 
+# a++ e ++a Não existe em Python
+a += 1
+# a-- e --a Não existe em Python
+a -= 1
+
+# Operadores Lógicos unários
+print(not 0)
+print(not 1)
+print(not -2)
+print(not False)
+print(not not True)
+
+
