2df7bbd77c36b19c5d330e2ac9a4b11811c75611
felipeafbruno <felipedobru@gmail.com>
2021-02-24 19:49:00 -0300

    Exercício 8 - atilizando for sem o else

--- estruturas_controle/switch_2.py
+++ estruturas_controle/switch_2.py
@@ -10,10 +10,10 @@ def get_dias_semana(dia):
     }
     return dias.get(dia)
 
+
 for dia in range(1, 8):
     dia_retornado = get_dias_semana(dia)
     if dia_retornado == 'Domingo' or dia_retornado == 'Sabado':
         print(f'{dia_retornado} é final de semana!')
     elif dia_retornado == 'Segunda' or dia_retornado == 'Terça' or dia_retornado == 'Quarta' or dia_retornado == 'Quinta' or dia_retornado == 'Sexta':
         print(f'{dia_retornado} é dia de semana!')
-   
