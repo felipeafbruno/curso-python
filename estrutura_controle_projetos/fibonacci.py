c4703c0826fe3737d8dbcb990bd243acf8c7e35d
felipeafbruno <felipedobru@gmail.com>
2021-02-25 09:25:20 -0300

    Exercício 1 - versão 1 do exercicio um  Exemplo de Fibonacci sem condição de parada

---
+++ estrutura_controle_projetos/fibonacci.py
@@ -0,0 +1,15 @@
+# Primeira versão do projeto
+# Fibonacci sem condição de parada
+def fibonacci():
+    penultimo = 0
+    ultimo = 1
+    print(f'{penultimo},{ultimo}', end=",")
+    while True:
+        proximo = penultimo + ultimo
+        print(proximo, end=",")
+        penultimo = ultimo
+        ultimo = proximo
+
+
+if __name__ == '__main__':
+    fibonacci()
