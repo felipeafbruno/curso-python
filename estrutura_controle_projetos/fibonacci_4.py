844396248e4e97dfb88b0c62d7a72f8c965c3a8e
felipeafbruno <felipedobru@gmail.com>
2021-02-25 09:53:24 -0300

    Exercício 4 - versão 4 do exercicio um Exemplo de Fibonacci utilizando lista e função com retorna

--- estrutura_controle_projetos/fibonacci_4.py
+++ estrutura_controle_projetos/fibonacci_4.py
@@ -1,10 +1,12 @@
-# Segunda versão do projeto
-# Fibonacci com limite definido na condição do laço while
+# Quarta versão do projeto
+# Fibonacci com limite definido na condição do laço while 
 def fibonacci(limite):
-    resultado = [0, 1]
+    resultado = [0, 1] # Lista
     while resultado[-1] < limite: # verificando se o valor da ultima posição da lista é maior que o limite imposto
         resultado.append(resultado[-2] + resultado[-1]) # penultima posição + ultima posição
+    return resultado
 
 
 if __name__ == '__main__':
-    fibonacci(21)
+    for valor_fibonacci in fibonacci(100):
+        print(valor_fibonacci)
