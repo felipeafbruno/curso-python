98001361d699f08758b9ed547b3f8dc42838234c
felipeafbruno <felipedobru@gmail.com>
2021-02-25 15:58:20 -0300

    Exercicio 2 - List Comprehension com condicional

--- list_comprehension/comprehension_v2.py
+++ list_comprehension/comprehension_v2.py
@@ -2,7 +2,7 @@
 dobros_dos_pares = [ i * 2 for i in range(10) if i % 2 == 0 ]
 print(dobros_dos_pares)
 
-# Versão Normal
+# Versão "normal"
 dobros_dos_pares = []
 for i in range(10):
     if i % 2 == 0:
