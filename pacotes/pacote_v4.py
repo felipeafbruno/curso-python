4f8266179b084d7383def0df0dff672886e9391f
felipeafbruno <felipedobru@gmail.com>
2021-02-26 14:59:06 -0300

    Exemplo de alias para a função de um módulo

---
+++ pacotes/pacote_v4.py
@@ -0,0 +1,6 @@
+from pacote1.module1 import somar
+from pacote2.module1 import subtracao as sub # um aalias/apelido para a função do módulo1 do pacote2
+
+
+print('Soma: ', somar(2,3))
+print('Subtração: ', sub(10,3))
