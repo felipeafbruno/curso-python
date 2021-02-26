bb29cf66788d5c5cc75d2eefa92e2a7802e52216
felipeafbruno <felipedobru@gmail.com>
2021-02-26 18:14:32 -0300

    Projeto Todo versão 3 - método mágico __iter__

---
+++ poo/todo_v3.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+
+
+class Projeto:
+    def __init__(self, nome):
+        self.nome = nome
+        self.lista_tarefas = []
+
+    # __iter__ -> permite que agora a classe Projeto seja iteravel 
+    # no caso a lista de tarefa
+    def __iter__(self): 
+        return self.lista_tarefas.__iter__()
+
+
+    def add(self, descricao):
+        self.lista_tarefas.append(Tarefa(descricao))
+
+
+    def pendentes(self):
+        return [tarefa for tarefa in self.lista_tarefas if not tarefa.feito]
+
+
+    def procurar(self, descricao):
+        # Possível IndexError
+        return [tarefa for tarefa in self.lista_tarefas if tarefa.descricao == descricao][0]
+
+
+    def __str__(self):
+        return f'{self.nome} ({len(self.pendentes())} tarefa(s) pendente(s))'
+
+
+class Tarefa:
+    def __init__(self, descricao):
+        self.descricao = descricao
+        self.feito = False
+        self.criacao = datetime.now()
+
+    def concluir(self):
+        self.feito = True
+
+    def __str__(self):
+        return self.descricao + ('(Concluída)' if self.feito else '')
+
+
+def main():
+    casa = Projeto('Tarefas de Casa')
+    casa.add('Brincar com o Bernardinho')
+    casa.add('Brincar com a Furtuninha')
+    casa.add('Terminar o módulo de POO')
+    casa.add('Lavar os Pratos')
+
+    casa.procurar('Lavar os Pratos').concluir()
+    for tarefa in casa:
+        print(f'- {tarefa}')
+        
+    print(casa)
+
+if __name__ == '__main__':
+    main()
