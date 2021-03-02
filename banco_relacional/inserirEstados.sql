4ec1b2b4ca4cc5e4d648a58c369f42786f433c66
felipeafbruno <felipedobru@gmail.com>
2021-03-02 17:19:54 -0300

    Insert de registros na tabela estados

---
+++ banco_relacional/inserirEstados.sql
@@ -0,0 +1,8 @@
+INSERT INTO estados (nome, sigla, regiao, populacao) VALUES ('Acre', 'AC', 'NORTE', 0.83);
+
+INSERT INTO estados 
+    (nome, sigla, regiao, populacao) 
+VALUES 
+    ('Alagoas', 'AL', 'NORDESTE', 3.38), 
+    ('Amapá', 'AP', 'NORTE', 0.08), 
+    ('Amazonas', 'AM', 'NORTE', 4.06);
