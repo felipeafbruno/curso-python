f59808d8133f6dc094711b2a6bec7ab8a2abe8e1
felipeafbruno <felipedobru@gmail.com>
2021-03-02 17:41:29 -0300

    Inserir registros na tabela cidades

---
+++ banco_relacional/inserirCidades.sql
@@ -0,0 +1,14 @@
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Campinas', 795, 25);
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Niterói', 133.9, 19);
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Caruaru', 133.9, (SELECT id FROM estados WHERE sigla = "PE"));
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Juazeiro do Norte', 248.2, (SELECT id FROM estados WHERE sigla = "CE"));
+
+SELECT * FROM cidades;
+SELECT * FROM estados;
