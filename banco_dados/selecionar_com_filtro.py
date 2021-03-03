de12a16564d6d312006442c7720eef6a60053bc2
felipeafbruno <felipedobru@gmail.com>
2021-03-03 14:20:28 -0300

    Select com where e operador '='

---
+++ banco_dados/selecionar_com_filtro.py
@@ -0,0 +1,12 @@
+from db import nova_conexao
+
+
+sql = "SELECT * FROM contatos WHERE tel = '3333-4444'"
+
+
+with nova_conexao() as conexao:
+    cursor = conexao.cursor()
+    cursor.execute(sql)
+
+    for registro in cursor:
+        print(registro)
