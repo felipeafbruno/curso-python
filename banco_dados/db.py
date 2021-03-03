c100f1487e228d60ccd08eff2b48bdb79ec188aa
felipeafbruno <felipedobru@gmail.com>
2021-03-03 11:21:31 -0300

    Utils devolvendo a instância de conexao - utiliando decorator contextmanager

---
+++ banco_dados/db.py
@@ -0,0 +1,20 @@
+from mysql.connector import connect
+from contextlib import contextmanager
+
+
+parametros = dict(
+    host='localhost',
+    port='3306',
+    user='root',
+    passwd='root@root?',
+    auth_plugin='mysql_nativa_password'
+)
+
+@contextmanager # generator
+def nova_conexao():
+    conexao = connect(**parametros)
+    try:
+        yield conexao
+    finally:
+        if (conexao and conexao.is_connected()):
+            conexao.close()
