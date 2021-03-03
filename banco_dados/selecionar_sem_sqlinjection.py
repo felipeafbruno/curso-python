a7341871deccd3adeaf834bb4c767990155e72cd
felipeafbruno <felipedobru@gmail.com>
2021-03-03 14:39:46 -0300

    Select com filtro e parametro passado pelo usuário'

--- banco_dados/selecionar_sem_sqlinjection.py
+++ banco_dados/selecionar_sem_sqlinjection.py
@@ -7,6 +7,8 @@ sql = "SELECT * FROM contatos WHERE nome LIKE %s"
 with nova_conexao() as conexao:
     # recebendo do usuário o nome para a consulta
     nome = input('Contato localizar: ')
+    # o trecho '%{nome}%' impede que algo passado para o nome seja interpretado como um SQL
+    # aplicando caracteres de escape de forma que o valor passado sempre seja interpretado como string
     args = (f'%{nome}%', )
     
     cursor = conexao.cursor()
