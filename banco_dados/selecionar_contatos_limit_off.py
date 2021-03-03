5d99a349e35eff921bfc2e7759bdd7d883cb0bdb
felipeafbruno <felipedobru@gmail.com>
2021-03-03 15:51:03 -0300

    Selecionar contatos

--- banco_dados/selecionar_contatos_limit_off.py
+++ banco_dados/selecionar_contatos_limit_off.py
@@ -2,21 +2,17 @@ from mysql.connector.errors import ProgrammingError
 from db import nova_conexao
 
 
-sql = 'SELECT * FROM contatos'
+sql = 'SELECT * FROM contatos LIMIT %s OFFSET %s'
+args = (5, 8)
 
 
 with nova_conexao() as conexao:
     try:
         cursor = conexao.cursor()
-        cursor.execute(sql)
-        # cursor.fetchall(): 
-        # retorna uma tupla com todas as linhas
-        # do resultado da query SELECT
+        cursor.execute(sql, args)
         contatos = cursor.fetchall()
     except ProgrammingError as error:
         print(f'Erro: {error.msg}')
     else:
         for contato in contatos:
-            # '2d' - limita o valor para dois digitos
-            # '20s' - limite o texto para vinte digitos 
             print(f'{contato[2]:2d} - {contato[0]:10s} - Telefone: {contato[1]}')
